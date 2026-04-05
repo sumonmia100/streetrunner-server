25dd0434ee0df51f05900d12cf06f7cc6a2f6df3
md sumon mia <engineercse19@gmail.com>
2026-04-05 16:56:43 +0600

    bug fix

--- server.js
+++ server.js
@@ -82,6 +82,6 @@ wss.on('connection', (ws) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`StreetRunner Server running on Port: ${PORT}`);
 });
